358de89fea814629a536fe59d50b4bffc19fab97
ITPro-Hemedi <hemedicharles@gmail.com>
2025-05-30 15:10:09 +0200

    updated files

---
+++ build/js/cart.js
@@ -0,0 +1,27 @@
+function getLocalStorage(a) {
+  return JSON.parse(localStorage.getItem(a));
+}
+function getCartContents() {
+  let a = "";
+  const r = getLocalStorage("so-cart"),
+    t = r.map((c) => renderCartItem(c));
+  document.querySelector(".product-list").innerHTML = t.join("");
+}
+function renderCartItem(a) {
+  const r = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${a.Image}"
+      alt="${a.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${a.Name}</h2>
+  </a>
+  <p class="cart-card__color">${a.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${a.FinalPrice}</p>
+</li>`;
+  return r;
+}
+getCartContents();
---
+++ src/js/product.js
@@ -0,0 +1,9 @@
+import ProductData from "./productData.js";
+import ProductDetails from "./productDetails.js";
+import { getParam } from "./utils.js";
+
+const productId = getParam("product");
+const dataSource = new ProductData("tents");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
